70a6c55ca3284c77a3ab0d69b70eefd6b7604085
James Sugrue <james.sugrue@gmail.com>
2015-01-03 07:37:15 +0000

    Fix issue #3 where the url was appended to each time a new search query was made.

--- chapter6/client/js/app/model/Search.js
+++ chapter6/client/js/app/model/Search.js
@@ -4,12 +4,13 @@ com.apress.model = com.apress.model || {};
 
 com.apress.model.Search = Backbone.Model.extend({
 
-	url : 'http://localhost:8080/search',
+	url 	: 'http://localhost:8080/search',
+	rootUrl : 'http://localhost:8080/search',
 
 
 	sync: function(method, model, options){
 		if(this.get('query')){
-			this.url = this.url + '/' + this.get('query');
+			this.url = this.rootUrl + '/' + this.get('query');
 		}
 		Backbone.sync.call(this, method, model, options);
 
